49a74fd5fc500c111ee3ea9e55e8c14a721fe5a0
grapgrap <fussaep@gmail.com>
2018-06-10 15:19:34 +0900

    put back predict score cache

--- algo/algo.js
+++ algo/algo.js
@@ -363,7 +363,7 @@ router.get('/predict-score', (req, res, next) => {
   ;
   database.query(q).subscribe(rows => {
     // 예상 평점을 계산한지 3일이 지나지 않았고, 그 값이 유효한 값이면 캐시된 데이터 전송
-    if (rows.length !== 0 && false) {
+    if (rows.length !== 0) {
       res.json({ result: 'success', data: rows[0].predicted_rate });
     } else {
       let sub = betterCBF(user_id, game_id).subscribe(result => {
